d5fff429721b40a643af566298079387d8487031
mixolex <fliparsenal@gmail.com>
2024-04-17 18:33:17 -0400

    First Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+## Integrate our OpenAI API
+import os
+from constants import openai_api_key
+from langchain.llms import OpenAI
+
+import streamlit as st
+
+os.environ["OPENAI_API_KEY"]=openai_api_key
+
+# streamlit framework
+
+st.title('Langchain Demo With OPENAI API')
+input_text = st.text_input("Search the topic you want")
+
+## OPENAI  LLMS
+llm=OpenAI(temperature=0.8)
+
+
+
+if input_text:
+    st.write(llm(input_text))
